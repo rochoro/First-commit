e7383b379b1e8b485e0affb892ad41d182f224a0
Ralph Ochoro <>
2022-06-22 20:17:45 +0000

    codes

---
+++ main.js
@@ -0,0 +1,5 @@
+document.getElementbyId(myButton).onclick = function () {
+  var personsname = document.getElementbyId("mytext").value;
+  console.log("Hello",personsname,"welcome to bootcamp");
+  
+}
